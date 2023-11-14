362f1e789d6b70d26057c8e02793a3d70cbbc081
Hennadii Borysevych <genuch3@gmail.com>
2023-11-15 00:11:55 +0100

    feat: register controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,34 @@
-const register = (req, res, next) => {};
+const db = require("../db");
+const bcrypt = require("bcrypt");
+
+const register = (req, res, next) => {
+  const { email, username, password } = req.body;
+
+  const q = "SELECT * FROM user WHERE email = ? OR username = ?";
+
+  db.query(q, [email, username], (err, data) => {
+    if (err) {
+      return res.json(err);
+    }
+
+    if (data.length) {
+      res.status(409).json("User already exists!");
+    }
+
+    const hashedPassword = bcrypt.hash(password, 10);
+
+    const queryUser =
+      "INSERT INTO users(`username`,`email`,`password`) VALUES (?)";
+
+    db.query(queryUser, [username, email, password], (err, data) => {
+      if (err) {
+        return res.json(err);
+      }
+      return res.status(201).json("User has been created.");
+    });
+  });
+};
+
 const login = (req, res, next) => {};
 const logout = (req, res, next) => {};
 
--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 
+const { login, register, logout } = require("../controllers/auth");
 
-router.post("/", ()=>{});
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", logout);
 
 module.exports = router;
